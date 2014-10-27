3d1be50f239020d6b64d20af91a692c7cec258bd
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-27 10:18:39 +0000

    Added a basic header and footer page (needs to be customized)

---
+++ website/inc.footer.php
@@ -0,0 +1,19 @@
+<?php 
+
+//This file is used as the template footer in all pages
+
+//Settable Variables:
+
+?>
+	</div>
+
+</div><!-- /.container -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
--- website/inc.header.php
+++ website/inc.header.php
@@ -1,3 +1,15 @@
+<?php 
+
+//This file is used as the template header in all pages
+
+//Settable Variables:
+//$pageTitle = "title of the page in header
+
+if(unset($pageTitle)) {
+    $pageTitle = "Unset, needs to be set";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +18,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
 
-	<title>Page Title</title>
+	<title><?=$pageTitle?></title>
 
 	<!-- Bootstrap core CSS -->
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 
 	<!-- CSS for the assignment Brief -->
-	<link href="css/website.css" rel="stylesheet">
+	<link href="css/brief.css" rel="stylesheet">
 
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
@@ -31,12 +43,11 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand active" href="#">Page Title</a>
+			<a class="navbar-brand active">Title</a>
 		</div>
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
-				<li><a href="#">Link A</a></li>
-				<li><a href="#">Link B</a></li>
+				<li><a>Link</a></li>
 			</ul>
 		</div>
 	</div>
@@ -45,20 +56,3 @@
 <div class="container">
 
 	<div class="main_content">
-
-		<div class="page-header">
-			<h1>Template Header</h1>
-		</div>
-
-	</div>
-
-</div><!-- /.container -->
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="js/jquery-1.11.1.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="js/ie10-viewport-bug-workaround.js"></script>
-</body>
-</html>
