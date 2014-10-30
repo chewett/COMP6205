bb48ed8700d5f14ef372ac9a3370a9f0d19b26e2
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-30 11:23:31 +0000

    Added a basic display of the roles

--- website/adminRolePermissions.php
+++ website/adminRolePermissions.php
@@ -1,14 +1,41 @@
 <?php
 
+require_once "bootstrap.php";
+
 $pageTitle = 'Admin Role Permissions';
 require_once 'inc.header.php';
+
+$allRoles = $em->getRepository("Role")->findAll();
 
 ?>
 <h1>Admin - Role Permissions</h1>
 
 This allows you to change what permissions different roles have.
 
-Format to be discussed
+<table class="table">
+	<thead>
+		<tr>
+			<th>Role ID</th>
+			<th>Role Name</th>
+			<th>Role Description</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php
+	/** @var $role Role */
+	foreach($allRoles as $role) {
+		?>
+		<tr>
+			<td><?=$role->getIdRole()?></td>
+			<td><?=$role->getRolename()?></td>
+			<td><?=$role->getDescription()?></td>
+		</tr>
+		<?php
+	}
+	?>
+	</tbody>
+</table>
+
 
 <?php
 require_once 'inc.footer.php';
