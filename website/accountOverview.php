c8f4e552a2afc68b4279e9633ab7ba69f545f262
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-31 16:32:35 +0000

    Added a correct account overview page pulling from the db

--- website/accountOverview.php
+++ website/accountOverview.php
@@ -6,41 +6,59 @@ require_once "setup.php";
 $pageTitle = 'Account Overview';
 require_once 'inc/header.php';
 
+$bankAccounts = $em->getRepository("Bankaccount")->findBy(array("idUser" => $user->getIdUser()));
+
 ?>
 
 <h1>Account Overview</h1>
 
-<p>Here is a list of accounts that you own</p>
+<p>Here is a list of accounts that you own. If there is an issue with your account you should <a href="reportAnIssue.php">report an issue</a></p>
 
-<p>If there is an issue with your account you should <a href="reportAnIssue.php">report an issue</a></p>
+<?php
 
-<table class="table">
-	<thead>
-	<tr>
-		<th>Account ID</th>
-		<th>Account name</th>
-		<th>Account Balance</th>
-	</tr>
-	</thead>
-	<tbody>
-	<tr>
-		<td>1</td>
-		<td>Main</td>
-		<td>£-1000</td>
-	</tr>
-	<tr>
-		<td>2</td>
-		<td>Other</td>
-		<td>£20</td>
-	</tr>
-	<tr>
-		<td>3</td>
-		<td>checking</td>
-		<td>£50</td>
-	</tr>
-	</tbody>
-</table>
+if($bankAccounts) {
+
+	?>
+<h2>Bank Accounts</h2>
+
+	<table class="table">
+		<thead>
+		<tr>
+			<th>Account ID</th>
+			<th>Account Name</th>
+			<th>Account Type</th>
+			<th>Account Balance</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php
+		foreach ($bankAccounts as $bank) {
+			?>
+			<tr>
+				<td><?=$bank->getIdBankaccount()?></td>
+				<td><?=$bank->getType()?></td>
+				<td><?=$bank->getName()?></td>
+				<td>£<?=$bank->getBalance()?></td>
+			</tr>
+		<?php } ?>
+		</tbody>
+	</table>
+
+<?php
+
+}else{ ?>
+	<strong>You have no bank accounts</strong>
+	<?php
+}
+?>
+
+<h2>Open an account</h2>
+
+<p>If you wish to open a web account you can <a href="openNewAccount.php">open an account</a>.</p>
+
 
 
 <?php
+
+
 require_once 'inc/footer.php';
