658f459df6ff4829b872c47a79c1890032b2b5dc
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-31 15:49:12 +0000

    fixed file references

--- website/inc/doctrine_setup.php
+++ website/inc/doctrine_setup.php
@@ -3,12 +3,12 @@
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
-require_once "vendor/autoload.php";
+require_once __DIR__ ."/../vendor/autoload.php";
 
 $paths = array(__DIR__."/../Entity");
 $isDevMode = true;
 
-require_once 'inc/config.php';
+require_once __DIR__ .'/../inc/config.php';
 
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 $em = EntityManager::create($dbParams, $config);
