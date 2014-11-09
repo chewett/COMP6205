b38d1bdbf30e204a98312ada98c4ae8c1b5343f1
Christopher Hewett <chewett@hotmail.co.uk>
2014-11-09 13:45:06 +0000

    Added a table to display all issues

--- website/viewAllIssues.php
+++ website/viewAllIssues.php
@@ -9,51 +9,88 @@ if(!userHasPermission("admin_view_issues")) {
       redirectUnauthorized();
 }
 
+/** @var Issue[] $openIssues */
+$openIssues = $em->getRepository("Issue")->findBy(array("status" => 1));
+/** @var Issue[] $closedIssues */
+$closedIssues = $em->getRepository("Issue")->findBy(array("status" => 1));
 
 require_once 'inc/header.php';
 
 ?>
 
-<h1>Admin - View all Concerns</h1>
+<h1>Admin - View all Issues</h1>
 
 This allows you to view all issues
 
 <h2>Open Issues</h2>
 
-<table class="table">
-	<thead>
-	<tr>
-		<th>Issue ID</th>
-		<th>User Reported</th>
-		<th>Title</th>
-		<th></th>
-	</tr>
-	</thead>
-	<tbody>
-	<tr>
-		<td>1</td>
-		<td>Bob</td>
-		<td>All my money has gone</td>
-		<td><a>View</a></td>
-	</tr>
-	<tr>
-		<td>2</td>
-		<td>Dave</td>
-		<td>I sent money to the wrong account</td>
-		<td><a>View</a></td>
-	</tr>
-	<tr>
-		<td>3</td>
-		<td>Mike</td>
-		<td>I dont know how to use this</td>
-		<td><a>View</a></td>
-	</tr>
-	</tbody>
-</table>
+<?php
+if($openIssues) {
+	?>
+	<table class="table">
+		<thead>
+		<tr>
+			<th>Issue ID</th>
+			<th>User Reported</th>
+			<th>Title</th>
+			<th></th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php
+		foreach($openIssues as $issue) {
+			?>
+			<tr>
+				<td><?=$issue->getIdIssue()?></td>
+				<td><?=$issue->getIdUser()->getUsername()?></td>
+				<td><?=$issue->getTitle()?></td>
+				<td><a href="viewIssue.php?id=<?=$issue->getIdIssue()?>">View</a></td>
+			</tr>
+			<?php
+		}
+		?>
+
+		</tbody>
+	</table>
+	<?php
+}
+?>
+
 
 <h2>Resolved Issues</h2>
 
-Similar table above
+
+<?php
+if($closedIssues) {
+	?>
+	<table class="table">
+		<thead>
+		<tr>
+			<th>Issue ID</th>
+			<th>User Reported</th>
+			<th>Title</th>
+			<th></th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php
+		foreach($closedIssues as $issue) {
+			?>
+			<tr>
+				<td><?=$issue->getIdIssue()?></td>
+				<td><?=$issue->getIdUser()->getUsername()?></td>
+				<td><?=$issue->getTitle()?></td>
+				<td><a href="viewIssue.php?id=<?=$issue->getIdIssue()?>">View</a></td>
+			</tr>
+		<?php
+		}
+		?>
+
+		</tbody>
+	</table>
+<?php
+}
+?>
 
 <?php
 require_once 'inc/footer.php';
