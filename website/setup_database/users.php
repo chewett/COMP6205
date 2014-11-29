5aafc64a694889d50fccda2269be0b622db9c749
Christopher Hewett <chewett@hotmail.co.uk>
2014-11-29 13:48:27 +0000

    Added a second bob account

--- website/setup_database/users.php
+++ website/setup_database/users.php
@@ -9,9 +9,16 @@ $bob->setPassword(generateHash("bob"));
 $bob->setLastname("Bob");
 $bob->setUsername("bob");
 
+$bob2 = new Users();
+$bob2->setFirstname("bob2");
+$bob2->setPassword(generateHash("bob2"));
+$bob2->setLastname("Bob2");
+$bob2->setUsername("bob2");
+
 
 
 $em->persist($bob);
+$em->persist($bob2);
 $em->flush();
 
 echo "If there are no errors then this was setup properly";
